ef0e9174c5b3df3e8bc3a1d211993618cc1b6c9c
UsmannK <usmann96@gmail.com>
2016-02-21 11:09:16 -0600

    done with FireBase btns

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,16 @@
 console.log('js loaded');
 
-var Firebase = require("firebase");
-var myFirebaseRef = new Firebase("https://taptap-app.firebaseio.com/");
+var myFirebaseRef = new Firebase("https://taptap-app.firebaseio.com/users/test-user");
+console.log('js loaded 2');
+
+
+$(document).on("click", ".app", function(event) {
+	var app = event.target.id;
+  var isSet;
+  myFirebaseRef.once("value", function(snap) {
+    isSet = snap.val()[app];
+  });
+  var message = {};
+  message[app] = !isSet; 
+  myFirebaseRef.update(message);
+});
